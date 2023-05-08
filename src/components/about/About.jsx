e92aaef1308a229623d99de7ac9b66739c92ff69
JKiskisl <smicscl@gmail.com>
2023-05-08 14:08:26 +0300

    add neccesary files

---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./about.css";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About us</h2>
+    </section>
+  );
+};
+
+export default About;
