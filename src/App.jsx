92c0dd1ff85e72fd8bbb050d3d6cf8a24d665220
JKiskisl <smicscl@gmail.com>
2023-05-10 14:01:48 +0300

    team,test,pricing

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
+import Testimonials from "./components/testimonials/Testimonials";
+import Product from "./components/product/Product";
+import Team from "./components/team/Team";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import Pricing from "./components/pricing/Pricing";
 
 const App = () => {
   return (
@@ -13,6 +17,11 @@ const App = () => {
       <Header />
       <About />
 
+      <Product />
+
+      <Testimonials />
+      <Pricing />
+      <Team />
       <Contact />
       <Footer />
     </>
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./product.css";
+
+const Product = () => {
+  return (
+    <section id="#product">
+      <h5>Product</h5>
+    </section>
+  );
+};
+
+export default Product;
