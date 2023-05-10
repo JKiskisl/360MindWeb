17cf4e91243ae95b19edf42d1db077cc43d05747
JKiskisl <smicscl@gmail.com>
2023-05-10 14:07:24 +0300

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Nav from "./components/nav/Nav";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
-import Testimonials from "./components/testimonials/Testimonials";
+
 import Product from "./components/product/Product";
 import Team from "./components/team/Team";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Pricing from "./components/pricing/Pricing";
+
+import Whyus from "./components/whyus/Whyus";
 
 const App = () => {
   return (
@@ -19,7 +21,7 @@ const App = () => {
 
       <Product />
 
-      <Testimonials />
+      <Whyus />
       <Pricing />
       <Team />
       <Contact />
---
+++ src/components/whyus/Whyus.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./whyus.css";
+
+const Whyus = () => {
+  return (
+    <section id="whyus">
+      <h5>Whyus</h5>
+    </section>
+  );
+};
+
+export default Whyus;
