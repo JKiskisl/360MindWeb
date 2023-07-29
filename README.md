9700b32fce2b140bf0eb48b04eb8bcb21edc37e8
JKiskisl <smicscl@gmail.com>
2023-07-29 17:00:53 +0300

    zzzz

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 //
 
 Web for UAB 360 mind.
+
+to fix:
+
+Contact us ->
+1.highlight contactus nav bar when in contact us section
+2.Mail input correct font+color
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import "./nav.css";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const [activeButton, setActiveButton] = useState("home");
@@ -9,46 +10,69 @@ const Nav = () => {
   useEffect(() => {
     const sections = document.querySelectorAll("section");
     const handleScroll = () => {
+      let currentActiveButton = activeButton;
+
       sections.forEach((section) => {
         const bounding = section.getBoundingClientRect();
 
-        if (bounding.top - 200 <= 0 && bounding.bottom - 200 >= 0) {
-          const id = section.getAttribute("id");
-          setActiveButton(id);
+        if (bounding.top <= 150 && bounding.bottom >= 150) {
+          currentActiveButton = section.getAttribute("id");
         }
       });
+
+      setActiveButton(currentActiveButton);
     };
 
+    // Add 'DOMContentLoaded' listener to ensure all sections are available
+    window.addEventListener("DOMContentLoaded", handleScroll);
     window.addEventListener("scroll", handleScroll);
 
     return () => {
+      window.removeEventListener("DOMContentLoaded", handleScroll);
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [activeButton]);
+
+  const getHomeLink = () => {
+    if (["/signup", "/login"].includes(window.location.pathname)) {
+      return activeButton === "contact" ? "/" : `/#${activeButton}`;
+    } else {
+      return `/#${activeButton}`;
+    }
+  };
 
   return (
     <nav>
       <ul>
         <li className={activeButton === "home" ? "active" : ""}>
-          <a href="#home">Home</a>
+          <a href={getHomeLink()}>Home</a>
         </li>
         <li className={activeButton === "about" ? "active" : ""}>
-          <a href="#about">About us</a>
+          <a href="/#about">About us</a>
         </li>
         <li className={activeButton === "product" ? "active" : ""}>
-          <a href="#product">Product</a>
+          <a href="/#product">Product</a>
         </li>
         <li className={activeButton === "whyus" ? "active" : ""}>
-          <a href="#whyus">Why us</a>
+          <a href="/#whyus">Why us</a>
         </li>
         <li className={activeButton === "pricing" ? "active" : ""}>
-          <a href="#pricing">Pricing</a>
+          <a href="/#pricing">Pricing</a>
         </li>
         <li className={activeButton === "team" ? "active" : ""}>
-          <a href="#team">Team</a>
+          <a href="/#team">Team</a>
         </li>
         <li className={activeButton === "contact" ? "active" : ""}>
-          <a href="#contact">Contact</a>
+          <a href="/#contact">Contact</a>
+        </li>
+      </ul>
+
+      <ul className="ul-right">
+        <li className={activeButton === "login" ? "active" : ""}>
+          <Link to="/login">Login</Link>
+        </li>
+        <li className={activeButton === "signup" ? "active" : ""}>
+          <Link to="/signup">Signup</Link>
         </li>
       </ul>
     </nav>
